2bcaffa39ffe63f6d00dac9fa70c3671b9501f07
Student S.S <hhhhh@email.com>
2024-02-17 09:45:31 +0400

    katalog commit

---
+++ katalog.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRACTICA
+{
+    /// <summary>
+    /// Логика взаимодействия для katalog.xaml
+    /// </summary>
+    public partial class katalog : Window
+    {
+        public katalog()
+        {
+            InitializeComponent();
+        }
+
+        private void m_od(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zh_od(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void m_ob(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zh_ob(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void acces(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void v_s(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
